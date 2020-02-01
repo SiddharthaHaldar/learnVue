65f042a5029a96fc17044227637b6a690709d0b5
Apple <apple@Apples-MacBook-Air.local>
2020-02-01 19:58:16 +0530

    second commit

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 var app = new Vue({
 	el : "#vue-app",
 	data : {
-		name : "Sid"
+		name : "Sid",
+		job : 'Software Engineer',
+		website : "http://www.google.com",
+		time : "Morning",
+		show : true,
+		age : 22
+	},
+	methods : {
+		greet : function(time)
+		{
+			this.time = time;
+			return "GOOD "+this.time+" "+this.name;
+		},
+		increase : function()
+		{
+			this.age += 1;
+		},
+		decrease : function()
+		{
+			this.age -= 1;
+		}
 	}
 })
